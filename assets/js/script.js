b72322d1321364531514d9446edb9ff4d122ba5c
njheymann <nils.heymann@Hotmail.com>
2023-07-02 16:21:24 +1000

    More API added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,36 +4,57 @@
 
 // Event handlers
 
-// Airport location API
-
 // Location
-var airportLocation = ""; // Need to assign to an input
-var airportsAPIKey = `a0820d14-87e3-4ba3-b66d-96618ceefedb`;
-var airportsURL = `https://airlabs.co/api/v9/airports?iata_code=${airportLocation}&api_key=${airportsAPIKey}`;
+var airportLocation = "syd"; // Need to assign to an input
 
-fetch(airports)
+// Airlabs API Key
+var airLabsAPIKey = `a0820d14-87e3-4ba3-b66d-96618ceefedb`;
+
+// Airlabs location URL
+var airportsURL = `https://airlabs.co/api/v9/airports?iata_code=${airportLocation}&api_key=${airLabsAPIKey}`;
+
+// Airport location API
+fetch(airportsURL)
   .then((response) => response.json())
   .then((data) => {
     // Country data
-    var country = `Country: ` + data.response[0].country_code;
+    var country = `Departing Country: ` + data.response[0].country_code;
 
     // Airport Name
-    var airportName = `Airport: ` + data.response[0].name;
+    var airportName = `Departing Airport: ` + data.response[0].name;
     console.log(country);
     console.log(airportName);
-  });
-// IATA code data FROM airport location API
-var iataCode = data.response[0].iata_code;
-var flightStatsAPIKey = "0ae7bd57d8e9678e19a06cc4d91ce269";
-var flightStatsAppID = `e1beaacb`;
-var flightStatsURL = `https://api.flightstats.com/flex/schedules/rest/v1/json/from/ABQ/departing/2023/07/05/12?appId=${flightStatsAppID}&appKey=${flightStatsAPIKey}&codeType=${iataCode}`;
 
-fetch(flightStatsURL)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+    var iataCode = data.response[0].iata_code;
+    console.log(iataCode);
+    var date = "2023-07-01"; // Need to assign to calender input
+
+    // Flight info URL
+    var flightStatsURL = `https://airlabs.co/api/v9/routes?dep_iata=${iataCode}&date=${date}&api_key=${airLabsAPIKey}`;
+
+    // Flight info API
+    fetch(flightStatsURL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        var arrivalIATA = data.response[0].arr_iata;
+
+        // Airport location URL but with arrival queries from flight info
+        var arrivalAirportURL = `https://airlabs.co/api/v9/airports?iata_code=${arrivalIATA}&api_key=${airLabsAPIKey}`;
+
+        // Airport location API again but with the ARRIVAL info from the flight info api.
+        fetch(arrivalAirportURL)
+          .then((response) => response.json())
+          .then((data) => {
+            var arrivalCountry =
+              "Arrival Country: " + data.response[0].country_code;
+            var arrivalAirport = "Arrival Airport: " + data.response[0].name;
+
+            console.log(arrivalCountry);
+            console.log(arrivalAirport);
+          });
+      });
   })
-
   .catch((error) => {
     console.error(error);
   });
